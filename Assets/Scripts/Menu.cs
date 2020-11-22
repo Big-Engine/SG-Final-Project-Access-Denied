9813b86e1455e4137cf632117a0d7eb11b6deb55
ZGiraldo <zwgiraldo@my.waketech.edu>
2020-11-21 20:20:22 -0500

    Working Main Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject helpPanel;
+
+    public void OnClickPlayButton()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void OnClickHelpButton()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    public void OnClickBackButton()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
