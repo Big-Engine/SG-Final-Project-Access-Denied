2f29755ace1660319c51c60a9a4f174bbee62b56
ZGiraldo <zwgiraldo@my.waketech.edu>
2020-11-25 15:34:15 -0500

    GroundAI

---
+++ Assets/Scripts/EnemyGroundAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroundAI : MonoBehaviour
+{
+    public GameObject position1;
+    public GameObject position2;
+
+    public float speed;
+    Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = position1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    void MoveToTarget()
+    {
+
+
+        target.z = transform.position.z;
+        transform.position = Vector2.MoveTowards(transform.position, target, speed);
+        if (transform.position == target)
+        {
+            if (Vector2.Distance(transform.position, position1.transform.position)< 1)
+            {
+                target = position2.transform.position;
+            }
+            else if(Vector2.Distance(transform.position, position2.transform.position) < 1)
+            {
+                target = position1.transform.position;
+            }
+        }
+    }
+}
