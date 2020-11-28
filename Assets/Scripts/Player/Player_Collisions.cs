ed9003be1d5a5fd1f61c95900e8c0efc91944d22
ZGiraldo <zwgiraldo@my.waketech.edu>
2020-11-28 13:15:19 -0500

    Flying Enemy Melee, health pickup/Player health

---
+++ Assets/Scripts/Player/Player_Collisions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Collisions : MonoBehaviour
+{
+    public int playerHealth = 3;
+    public int maxHealth;
+
+    private void Start()
+    {
+        maxHealth = playerHealth;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            int magnitude = 1500;
+
+            Vector2 force = transform.position - other.transform.position;
+
+            force.Normalize();
+            GetComponent<Rigidbody2D>().AddForce(force * magnitude);
+
+            playerHealth--;
+            CheckIfDead();
+        }
+
+        if(other.tag == "Health")
+        {
+            if(maxHealth != playerHealth)
+            {
+                playerHealth++;
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    void CheckIfDead()
+    {
+        if (playerHealth <= 0)
+        {
+            //player dies
+            //change scene, animations, etc.
+            Destroy(gameObject);
+        }
+    }
+}
+
+
