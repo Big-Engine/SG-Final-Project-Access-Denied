fee6f2010189de1c8a16295750d44dc1d9713427
ZGiraldo <zwgiraldo@my.waketech.edu>
2020-12-02 09:45:04 -0500

    Created a 'spawner' prefab to be able to spawn flying enemies upon collision

---
+++ Assets/Scripts/SpawnTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Enemy1.SetActive(true);
+            Enemy2.SetActive(true);
+            Enemy3.SetActive(true);
+        }
+    }
+}
+
