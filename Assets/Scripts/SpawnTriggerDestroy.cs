5399886890d8e1b81ca5793f36aaed83f751a40a
ZGiraldo <zwgiraldo@my.waketech.edu>
2020-12-09 15:23:09 -0500

    Enemy spawner fix, ground enemy melee, platform fix

--- Assets/Scripts/SpawnTriggerDestroy.cs
+++ Assets/Scripts/SpawnTriggerDestroy.cs
@@ -2,20 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnTrigger : MonoBehaviour
+public class SpawnTriggerDestroy : MonoBehaviour
 {
-    public List<GameObject> Enemies = new List<GameObject>();
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            for(int i = 0; i < Enemies.Count; i++)
-            {
-                Enemies[i].SetActive(true);
-            }
             Destroy(gameObject);
         }
     }
 }
-
